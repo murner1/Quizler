09ddde9558cfed04b486df9e7294dc57393c07bf
murner1 <murner@mail.fresnostate.edu>
2022-06-04 02:50:23 -0700

    Implemented  chooseRandom

--- src/lib.js
+++ src/lib.js
@@ -1,7 +1,32 @@
 import fs from 'fs'
 
-export const chooseRandom = () => {
-  // TODO implement chooseRandom
+export const chooseRandom = (array = [], numItems) => {
+  if (array === undefined || array.length <= 1 || numItems <= 1) {
+    return array
+  } else if (
+    numItems === undefined ||
+    numItems <= 0 ||
+    numItems > array.length
+  ) {
+    numItems = Math.floor(Math.random() * array.length)
+  }
+
+
+  const randomIndices = Array(numItems) 
+    .fill() 
+    .reduce(res => {
+      let randomIndex = Math.floor(Math.random() * array.length)
+
+      while (res.indexOf(randomIndex) !== -1) {
+        randomIndex = Math.floor(Math.random() * array.length)
+      }
+
+      return res.concat(randomIndex)
+    }, [])
+
+  return randomIndices.map(individual => array[individual])
+
+  
 }
 
 export const createPrompt = () => {
